510f4980f0b57d85235eebe7b8b891a731d33b9a
Martyn Kemp <martyn@datasec1.co.uk>
2022-05-10 15:06:46 +0100

    Update script-b-pd.sh

--- script-b-pd.sh
+++ script-b-pd.sh
@@ -4,10 +4,10 @@ UUID=$(blkid /dev/nvme1n1 -s UUID | cut -d'=' -f2 | sed -e 's/^"//' -e 's/"$//')
 sudo echo "LABEL=cloudimg-rootfs   /        ext4   defaults,discard        0 1" > /etc/fstab 
 sudo echo "UUID=$UUID /data ext4 defaults 0 0" >> /etc/fstab
 sudo mount -a
-sudo rm -rf /data/chains/polkadot/network/
-sudo rm -rf /data/chains/polkadot/keystore/
+sudo rm -rf /data/chains/polkadot/network/*
+sudo rm -rf /data/chains/polkadot/keystore/*
 sudo cp /tmp/pd/n/* /data/chains/polkadot/network/
-cp /tmp/pd/k/* /data/chains/polkadot/keystore/ || echo "Empty"
+sudo cp /tmp/pd/k/* /data/chains/polkadot/keystore/ || echo "Empty"
 sudo docker restart $(sudo docker ps -aqf "name=polkadot")
 
 tail -f /data/nodestate/collector.log

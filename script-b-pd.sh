03e6d80ecb290ca724c4ddfffb808e0813c4e930
Martyn Kemp <martyn@datasec1.co.uk>
2022-05-11 22:02:15 +0100

    Update script-b-pd.sh

--- script-b-pd.sh
+++ script-b-pd.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-## this block for offline de/reattach
-#sudo cp /etc/fstab /tmp
-#UUID=$(blkid /dev/nvme1n1 -s UUID | cut -d'=' -f2 | sed -e 's/^"//' -e 's/"$//') 
-#sudo echo "LABEL=cloudimg-rootfs   /        ext4   defaults,discard        0 1" > /etc/fstab 
-#sudo echo "UUID=$UUID /data ext4 defaults 0 0" >> /etc/fstab
-#sudo mount -a
-
 sudo docker pause $(sudo docker ps -aqf "name=polkadot")
 cd /data/chains/polkadot/network/ && sudo rm -rf *
 cd /data/chains/polkadot/keystore/ && sudo rm -rf *

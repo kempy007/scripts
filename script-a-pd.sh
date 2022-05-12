3966ea324a37f269892dfe3502c84c9ce3e6b6bb
Martyn Kemp <martyn@datasec1.co.uk>
2022-05-12 08:52:49 +0100

    Update script-a-pd.sh

--- script-a-pd.sh
+++ script-a-pd.sh
@@ -3,3 +3,4 @@ sudo docker pause $(sudo docker ps -aqf "name=polkadot")
 sudo mkdir ~/pd && sudo mkdir ~/pd/n && sudo mkdir ~/pd/k
 sudo cp /data/chains/polkadot/network/* ~/pd/n
 sudo cp -r /data/chains/polkadot/keystore/* ~/pd/k || echo "Empty"
+sudo umount /data

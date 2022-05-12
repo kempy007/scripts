476aeb53f6a3f06ce93b7ced0257a767967369e9
Martyn Kemp <martyn@datasec1.co.uk>
2022-05-12 11:07:11 +0100

    Update script-b-pd.sh

--- script-b-pd.sh
+++ script-b-pd.sh
@@ -6,6 +6,8 @@ cd /data/chains/polkadot/keystore/ && sudo rm -rf *
 sudo cp ~/pd/n/* /data/chains/polkadot/network/
 sudo cp -r ~/pd/k/* /data/chains/polkadot/keystore/ || echo "Empty"
 
-#sudo docker restart $(sudo docker ps -aqf "name=polkadot")
+sudo systemctl start amazon-cloudwatch-agent
+sudo systemctl start grafana-agent
+sudo docker restart $(sudo docker ps -aqf "name=polkadot")
 
 #tail -f /data/nodestate/collector.log

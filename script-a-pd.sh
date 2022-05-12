8be0bbe06ba6f3d815889c2a853d8732b969b9ba
Martyn Kemp <martyn@datasec1.co.uk>
2022-05-12 09:33:51 +0100

    Update script-a-pd.sh

--- script-a-pd.sh
+++ script-a-pd.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo docker pause $(sudo docker ps -aqf "name=polkadot")
+sudo docker pause $(sudo docker ps -a -q)
 sudo mkdir ~/pd && sudo mkdir ~/pd/n && sudo mkdir ~/pd/k
 sudo cp /data/chains/polkadot/network/* ~/pd/n
 sudo cp -r /data/chains/polkadot/keystore/* ~/pd/k || echo "Empty"
+sudo systemctl stop amazon-cloudwatch-agent
+sudo systemctl stop grafana-agent
 sudo umount /data

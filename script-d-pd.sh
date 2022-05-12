48cb206966a427564f39cd5f8fd6e0db49a7fc90
Martyn Kemp <martyn@datasec1.co.uk>
2022-05-12 08:52:11 +0100

    Update script-d-pd.sh

--- script-d-pd.sh
+++ script-d-pd.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sudo cp /etc/fstab ~/
-UUID=$(blkid /dev/nvme1n1 -s UUID | cut -d'=' -f2 | sed -e 's/^"//' -e 's/"$//') 
+UUID=$(blkid /dev/nvme1n1 -s UUID | cut -d'=' -f2 | sed -e 's/^"//' -e 's/"$//')
+sudo chmod 777 /etc/fstab
 sudo echo "LABEL=cloudimg-rootfs   /        ext4   defaults,discard        0 1" > /etc/fstab 
 sudo echo "UUID=$UUID /data ext4 defaults 0 0" >> /etc/fstab
+sudo chmod 644 /etc/fstab
 #sudo mount -a

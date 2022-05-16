c03d99f4f5e2fa9daf31eff73b1924fc148ac726
Martyn Kemp <martyn@datasec1.co.uk>
2022-05-16 12:03:13 +0100

    Update script-d-pd.sh

--- script-d-pd.sh
+++ script-d-pd.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+update_mounts {
+  sudo cp /etc/fstab ~/
+  sudo chmod 777 /etc/fstab
+  sudo echo "LABEL=cloudimg-rootfs   /        ext4   defaults,discard        0 1" > /etc/fstab 
+  sudo echo "UUID=$UUID /data ext4 defaults 0 0" >> /etc/fstab
+  sudo chmod 644 /etc/fstab
+  sudo mount -a
+  sudo mount | grep ' /data'
+}
+
 ## if we want to regen the uuid > https://www.simplified.guide/linux/disk-uuid-set
 UUID=$(sudo blkid /dev/nvme1n1 -s UUID | cut -d'=' -f2 | sed -e 's/^"//' -e 's/"$//')
 echo "UUID=$UUID"
@@ -11,13 +21,3 @@ else
   echo "@@@@@@@@@@@@@@@@@@@@@@@@"
   echo "No UUID, attach failure"
 fi
-
-update_mounts () {
-  sudo cp /etc/fstab ~/
-  sudo chmod 777 /etc/fstab
-  sudo echo "LABEL=cloudimg-rootfs   /        ext4   defaults,discard        0 1" > /etc/fstab 
-  sudo echo "UUID=$UUID /data ext4 defaults 0 0" >> /etc/fstab
-  sudo chmod 644 /etc/fstab
-  sudo mount -a
-  sudo mount | grep ' /data'
-}

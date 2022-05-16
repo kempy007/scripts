26a4e9e57537c15b27ab09e3581bb1481fa6ddcb
Martyn Kemp <martyn@datasec1.co.uk>
2022-05-16 09:56:25 +0100

    Update script-b-pd.sh

--- script-b-pd.sh
+++ script-b-pd.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-sudo docker kill $(sudo docker ps -aqf "name=polkadot")
-cd /data/chains/polkadot/network/ && sudo rm -rf *
-cd /data/chains/polkadot/keystore/ && sudo rm -rf *
-sudo cp ~/pd/n/* /data/chains/polkadot/network/
-sudo cp -r ~/pd/k/* /data/chains/polkadot/keystore/ || echo "Empty"
+if [[ $(sudo mount | grep ' /data') ]]; then 
+  echo "Mount Ok"
+  restore_services
+else
+  echo "@@@@@@@@@@@@@@@"
+  echo "No mounts found"
+fi
 
-sudo systemctl start amazon-cloudwatch-agent
-sudo systemctl start grafana-agent
-sudo systemctl start nodestatebeat
-sudo docker kill $(sudo docker ps -aqf "name=polkadot")
-sudo docker kill $(sudo docker ps -aqf "name=collector")
-sleep 2
-sudo docker start $(sudo docker ps -aqf "name=polkadot")
-sudo docker start $(sudo docker ps -aqf "name=collector")
-sudo docker start $(sudo docker ps -a -q)
-sudo docker unpause $(sudo docker ps -a -q)
+restore_services () {
+  sudo docker kill $(sudo docker ps -aqf "name=polkadot")
+  cd /data/chains/polkadot/network/ && sudo rm -rf *
+  cd /data/chains/polkadot/keystore/ && sudo rm -rf *
+  sudo cp ~/pd/n/* /data/chains/polkadot/network/
+  sudo cp -r ~/pd/k/* /data/chains/polkadot/keystore/ || echo "Empty"
 
-#tail -f /data/nodestate/collector.log
+  sudo systemctl start amazon-cloudwatch-agent
+  sudo systemctl start grafana-agent
+  sudo systemctl start nodestatebeat
+  sudo docker kill $(sudo docker ps -aqf "name=polkadot")
+  sudo docker kill $(sudo docker ps -aqf "name=collector")
+  sleep 2
+  sudo docker start $(sudo docker ps -aqf "name=polkadot")
+  sudo docker start $(sudo docker ps -aqf "name=collector")
+  sudo docker start $(sudo docker ps -a -q)
+  sudo docker unpause $(sudo docker ps -a -q)
+}

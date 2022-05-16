7df170b6533cfc639832f4760b7580c19dff2dad
Martyn Kemp <martyn@datasec1.co.uk>
2022-05-16 12:06:14 +0100

    Update script-d-pd.sh

--- script-d-pd.sh
+++ script-d-pd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-update_mounts {
+function update_mounts {
   sudo cp /etc/fstab ~/
   sudo chmod 777 /etc/fstab
   sudo echo "LABEL=cloudimg-rootfs   /        ext4   defaults,discard        0 1" > /etc/fstab 

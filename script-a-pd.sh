968c06f1b23d7d5df215c8ef288c0239db3a2853
Martyn Kemp <martyn@datasec1.co.uk>
2022-05-17 09:20:13 +0100

    Update script-a-pd.sh

--- script-a-pd.sh
+++ script-a-pd.sh
@@ -8,23 +8,27 @@ if [ -n "$KEEPKEYS" ]; then
     sudo cp -r /data/chains/polkadot/keystore/* ~/pd/k || echo "Empty"
 fi
 
-sudo systemctl stop amazon-cloudwatch-agent
-sudo systemctl stop grafana-agent
-sudo systemctl stop nodestatebeat
-sudo docker stop $(sudo docker ps -a -q)
-sudo lsof | grep ' /data' || sudo apt-get -y install lsof && sudo lsof | grep ' /data'
-touch ~/mnt.in.use
-echo "Mount in use..."
-while [ -f ~/mnt.in.use ];
-do
-    if [[ $(sudo lsof | grep ' /data') ]]; then 
-      echo "."
-    else
-      rm ~/mnt.in.use 
-    fi
-    sleep 1
-done
-sudo lsof | grep ' /data'
-sleep 3
-
-sudo umount /data
+if [[ $(lsblk | grep ' /data') ]]; then
+    echo "Attempting to unmount /data cleanly"
+    sudo systemctl stop amazon-cloudwatch-agent
+    sudo systemctl stop grafana-agent
+    sudo systemctl stop nodestatebeat
+    sudo docker stop $(sudo docker ps -a -q)
+    sudo lsof | grep ' /data' || sudo apt-get update && sudo apt-get -y install lsof && sudo lsof | grep ' /data'
+    touch ~/mnt.in.use
+    echo "Mount in use..."
+    while [ -f ~/mnt.in.use ];
+    do
+        if [[ $(sudo lsof | grep ' /data') ]]; then 
+          echo "."
+        else
+          rm ~/mnt.in.use 
+        fi
+        sleep 1
+    done
+    sudo lsof | grep ' /data'
+    sleep 3
+    sudo umount /data
+else
+    echo "No /data mounts; skipping"
+fi
